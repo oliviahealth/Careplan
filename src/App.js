9bf05d86f104898f1b03ae5f2ec7823cb6837976
Naimish Kavani <naimishkavani@client-10-229-199-104.tamulink.tamu.edu>
2023-09-21 12:06:54 -0500

    Initial commit with front end code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import SignInSide from './SignInSide';
+import SignUp from './SignUp';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [data, setData] = useState([{}]);
+
+  useEffect(() => {
+    // ... (your fetch code remains unchanged)
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Router>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="#home">My Website</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/">Sign In</Nav.Link>
+            <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
+            {/* ... other nav links ... */}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Routes>
+        <Route path="/" element={<SignInSide />} />
+        <Route path="/signup" element={<SignUp />} />
+        {/* ... other routes ... */}
+      </Routes>
+    </Router>
   );
 }
 
