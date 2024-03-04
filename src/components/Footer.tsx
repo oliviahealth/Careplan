e3c7d0335880a9e753a4330f4834d4d4f4e728c0
Kunal Jain <55034053+wherewith@users.noreply.github.com>
2024-03-04 00:58:50 -0600

    update footer date

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
     return (
         <div className="flex bg-[#500000] text-white text-xs md:py-1 font-OpenSans">
             <div className="w-3/4 mx-auto flex justify-center items-center [&>a]:m-2">
-                <a href="https://www.tamu.edu" target="_blank">© 2023 TEXAS A&M UNIVERSITY</a>
+                <a href="https://www.tamu.edu" target="_blank">© 2024 TEXAS A&M UNIVERSITY</a>
                 <a href="https://it.tamu.edu/site-policies.php" target="_blank">SITE POLICIES</a>
                 <a href="https://itaccessibility.tamu.edu/" target="_blank">WEB ACCESSIBILITY</a>
             </div>
