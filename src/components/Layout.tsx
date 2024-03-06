89ddacb46c1c2ce7666445e17d8a15a2146741c3
Sumit Nalavade <vs.nalavade2003@gmail.com>
2024-03-05 22:26:59 -0600

    Refactoring layout to use Outlet

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,14 @@
+import { Outlet } from "react-router-dom";
+
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+
 
 const Layout = () => {
   return (
     <div className="flex flex-col h-[100vh]">
       <Navbar />
-      <div className="flex flex-grow overflow-hidden bg-[url('./images/background.png')] bg-cover justify-center items-center" role="img" aria-label="A background image of a mother and her baby">
-        <img
-          className="w-[50rem]"
-          src="/images/oliviahealth.svg"
-          alt="Olivia Health logo"
-          style={{ filter: "brightness(0) invert(1)" }}
-        ></img>
-      </div>
+        <Outlet />
       <Footer />
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from './App.tsx'
+import LandingPAge from './components/LandingPage.tsx'
 import './index.css'
+
+import Layout from './components/Layout.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />}/>
+        <Route element={<Layout />}>
+          <Route path='/' element={<LandingPAge />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
