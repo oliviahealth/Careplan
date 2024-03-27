255587de9930a8089715c1262a0f915be51a60ed
Kunal Jain <55034053+wherewith@users.noreply.github.com>
2024-03-27 12:56:16 -0500

    update sizes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer: React.FC = () => {
     return (
-        <div className="flex bg-[#500000] text-white md:text-xs text-[0.5rem] md:py-1 font-OpenSans">
-            <div className="w-3/4 mx-auto flex justify-center items-center [&>a]:m-1 md:[&>a]:m-2">
+        <div className="flex min-h-auto bg-[#500000] text-white text-[.75rem] md:text-sm font-OpenSans">
+            <div className="w-7/8 md:w-3/4 mx-auto flex justify-center items-center gap-4 p-3">
                 <a href="https://www.tamu.edu" target="_blank">© 2024 TEXAS A&M UNIVERSITY</a>
                 <a href="https://it.tamu.edu/site-policies.php" target="_blank">SITE POLICIES</a>
                 <a href="https://itaccessibility.tamu.edu/" target="_blank">WEB ACCESSIBILITY</a>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,23 +9,23 @@ const Navbar: React.FC = () => {
   }
 
   return (
-    <div className="flex min-h-[6.25rem] w-full items-center text-black border-b shadow-xl">
+    <div className="flex min-h-[4.5rem] w-full items-center text-black border-b shadow-xl">
       <div className="flex w-[60rem] px-4 mx-auto items-center justify-between">
         <div>
           <Link to={"/"}>
-            <img className="h-auto" alt="Texas A&M Olivia Health Plan of Safecare logo" src="/images/pageone.svg"></img>
+            <img className="h-[3rem]" alt="Texas A&M Olivia Health Plan of Safecare logo" src="/images/pageone.svg"></img>
           </Link>
         </div>
 
         <div>
-        <div className="md:hidden selectable group" onClick={menuToggle}>
+        <div className="md:hidden selectable group"onClick={menuToggle}>
           <div className="space-y-2">
             <span className="block h-1 w-8 bg-black rounded-full"></span>
             <span className="block h-1 w-8 bg-black rounded-full"></span>
             <span className={`block h-1 w-4 bg-black rounded-full transition-all duration-200 ease-out group-hover:w-8 ${isOpen ? "w-8" : ""}`}></span>
           </div>
         </div>
-        <div className={`${isOpen ? "block bg-white border shadow mt-8 mr-1" : "hidden"} absolute rounded-xl md:shadow-none md:bg-none md:border-0 md:relative right-0 md:mt-0 p-5 md:p-0 md:flex space-y-6 md:space-y-0 md:space-x-4`}> 
+        <div className={`${isOpen ? "block bg-white border shadow mt-4 mr-1" : "hidden"} absolute rounded-xl md:shadow-none md:bg-none md:border-0 md:relative right-0 md:mt-0 p-4 md:p-0 md:flex space-y-6 md:space-y-0 md:space-x-4 text-sm md:text-base`}> 
           <Link to={"/"} className="block md:flex button">
             Client Dashboard
           </Link>
