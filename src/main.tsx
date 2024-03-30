c1beb4627bb61dd71799d956f992c398fd87f08a
vedarth31 <vedarth31@gmail.com>
2024-03-30 12:29:12 -0500

    added integration for maternal-demographics with backend

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import './index.css'
 
 import Layout from './components/Layout.tsx';
@@ -12,19 +14,22 @@ import MaternalDemographics from './components/MaternalDemographics.tsx';
 import MaternalMedicalHistory from './components/MaternalMedicalHistory.tsx';
 import MaternalPsychiatricHistory from './components/MaternalPsychiatricHistory.tsx';
 
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path='/' element={<LandingPage />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/maternal-demographics' element={<MaternalDemographics />} />
-          <Route path='/maternal-medical-history' element={<MaternalMedicalHistory />} />
-          <Route path='/maternal-psychiatric-history' element={<MaternalPsychiatricHistory />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-</React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path='/' element={<LandingPage />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/maternal-demographics' element={<MaternalDemographics />} />
+            <Route path='/maternal-medical-history' element={<MaternalMedicalHistory />} />
+            <Route path='/maternal-psychiatric-history' element={<MaternalPsychiatricHistory />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>
 )
