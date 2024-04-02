83cbfab59854dad02b18d97ea0a6736e972f7027
Sumit Nalavade <vs.nalavade2003@gmail.com>
2024-04-02 02:45:18 -0500

    Minor changes

--- src/components/MaternalMedicalHistory.tsx
+++ src/components/MaternalMedicalHistory.tsx
@@ -59,23 +59,22 @@ export default function MaternalMedicalHistory() {
 
     const deliveryModes = ["Vaginal", "Cesarean"];
 
-    const numberOptions =  [];
+    const numberOptions = [];
     for (let i = 0; i <= 10; i++) {
         numberOptions.push(<option key={i} value={i}>{i}</option>);
     }
-    
 
-    return(
+
+    return (
         <div className="flex justify-center w-full p-2 mt-2 text-base font-OpenSans">
             <form onSubmit={handleSubmit(onSubmit)} className="w-[40rem] md:w-[30rem] m-5 md:m-0 space-y-1 [&>p]:pt-6 [&>p]:pb-1 [&>input]:px-4">
                 <p className="font-medium text-xl whitespace-nowrap">Prenatal Care (for current or most recent pregnancy)</p>
-                <div className="flex flex-nowrap space-x-4">
+                <div className="space-y-7">
                     <div className="flex flex-col flex-grow">
                         <p className="font-medium">Gestation Age at Entry of Care</p>
                         <select {...register("gestationAge")} className="border border-gray-300 px-4 py-2 rounded-md w-full">
                             {[...Array(100).keys()].map(age => (<option key={age} value={age}>{age}</option>))}
                         </select>
-
                     </div>
                     <div className="flex flex-col flex-grow">
                         <p className="font-medium">Anticipated Delivery Date</p>
@@ -112,23 +111,23 @@ export default function MaternalMedicalHistory() {
                 </div>
 
                 <p className="font-medium">Date Completed</p>
-                <input {...register("dateCompleted")} className="border border-gray-300 px-4 py-2 rounded-md w-full" type="date"/>
+                <input {...register("dateCompleted")} className="border border-gray-300 px-4 py-2 rounded-md w-full" type="date" />
 
                 <p className="font-medium text-xl">Obstretric History</p>
 
                 <p className="font-medium">Total Number of Pregnancies</p>
-                <select {...register("numPregnancies")}className="dropdown border rounded-md border-gray-300 p-3 font-medium">
-                    {Array.from({ length: 6}, (_, i) => (<option key={i} value={i}>{i}</option>))}
+                <select {...register("numPregnancies")} className="dropdown border rounded-md border-gray-300 p-3 font-medium">
+                    {Array.from({ length: 6 }, (_, i) => (<option key={i} value={i}>{i}</option>))}
                 </select>
 
                 <p className="font-medium">Number of Live Births</p>
                 <select {...register("numLiveBirths")} className="dropdown border rounded-md border-gray-300 p-3 font-medium">
-                    {Array.from({ length: 6}, (_, i) => (<option key={i} value={i}>{i}</option>))}
+                    {Array.from({ length: 6 }, (_, i) => (<option key={i} value={i}>{i}</option>))}
                 </select>
 
                 <p className="font-medium">Number of Children Living with Mother</p>
                 <select {...register("numChildrenLivingWithMother")} className="dropdown border rounded-md border-gray-300 p-3 font-medium">
-                    {Array.from({ length: 6}, (_, i) => (<option key={i} value={i}>{i}</option>))}
+                    {Array.from({ length: 6 }, (_, i) => (<option key={i} value={i}>{i}</option>))}
                 </select>
 
                 <p className="font-medium">Please Explain Complications During Prior Pregnancies</p>
@@ -140,12 +139,8 @@ export default function MaternalMedicalHistory() {
                 <textarea {...register("diagnoses")} className="border border-gray-300 px-4 py-2 rounded-md w-full" />
 
                 <p className="font-medium text-xl">Current Medication List</p>
-                <div className="flex justify-center">
-                    <button type="button" onClick={addNewMedication} className="text-black px-20 py-2 mt-6 rounded-md whitespace-nowrap">+ Add Medication</button>
-                    <button type="button" onClick={removeLastMedication} className="text-red-600 px-20 py-2 mt-6 rounded-md whitespace-nowrap" disabled={fields.length === 0}>- Remove Medication</button>
-                </div>
                 {fields.map((field, index) => (
-                    <div key={field.id} className="py-6">
+                    <div key={field.id} className="py-4">
                         <p className="font-medium pt-6">Medication {index + 1}</p>
                         <input {...register(`medications.${index}.medication`)} className="border border-gray-300 px-4 py-2 rounded-md w-full" />
 
@@ -167,7 +162,10 @@ export default function MaternalMedicalHistory() {
                         <textarea {...register(`medications.${index}.medicationNotes`)} className="border border-gray-300 px-4 py-2 rounded-md w-full" />
                     </div>))}
 
-
+                <div className="flex justify-center">
+                    <button type="button" onClick={addNewMedication} className="text-black px-20 py-2 mt-6 rounded-md whitespace-nowrap">+ Add Medication</button>
+                    <button type="button" onClick={removeLastMedication} className="text-red-600 px-20 py-2 mt-6 rounded-md whitespace-nowrap" disabled={fields.length === 0}>- Remove Medication</button>
+                </div>
 
                 <p className="font-medium">Other Notes</p>
                 <textarea {...register("otherNotes")} className="border border-gray-300 px-4 py-2 rounded-md w-full" />
