8b17e49383fd675680fc8072bd511387dde4a74e
Vedarth Atreya <83242510+vedarth31@users.noreply.github.com>
2024-04-21 20:45:46 -0500

    updates to MaternalDemographics and MaternalMedicalHistory forms (#16)
    * maternaldemographics and medicalhistory forms populate with existing data
    
    * fixed error with attended_postpartum_visit field
    
    * removed obgyn field from MaternalMedicalHistory; added text at top
    
    ---------
    
    Co-authored-by: Sumit Nalavade <vs.nalavade2003@gmail.com>

--- src/components/MaternalMedicalHistory.tsx
+++ src/components/MaternalMedicalHistory.tsx
@@ -3,6 +3,7 @@ import { useMutation } from 'react-query'
 import axios from 'axios'
 import { z } from 'zod'
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react"
 
 const CurrentMedicationList = z.object({
     name: z.string().min(1, 'Name is required'),
@@ -10,6 +11,19 @@ const CurrentMedicationList = z.object({
     prescriber: z.string().min(1, 'Prescriber is required'),
     notes: z.string().min(1, 'Notes is required')
 });
+
+const deliveryModesEnum = z.enum([
+    "Vaginal",
+    "Cesarean"
+]);
+const deliveryModes = Object.values(deliveryModesEnum.Values);
+
+const attendedPostpartumEnum = z.enum([
+    "Yes",
+    "No"
+]);
+const attendedPostpartum = Object.values(attendedPostpartumEnum.Values);
+
 const MaternalMedicalHistoryInputs = z.object({
     gestational_age: z.string().min(1, 'Gestational age is required'),
     anticipated_delivery_date: z.string().min(1, 'Anticipated delivery date is required'),
@@ -23,10 +37,10 @@ const MaternalMedicalHistoryInputs = z.object({
     total_num_children_with_mother: z.string().min(1, 'Total number of children with mother is required'),
     prior_complications: z.string().default(""),
     current_medication_list: z.array(CurrentMedicationList),
-    med_problems_diagnoses: z.string().default(""),
-    notes: z.string().default(""),
+    med_problems_diagnoses: z.string().min(1, 'required'),
+    notes: z.string().min(1, 'Notes is required'),
 });
-type MaternalMedicalHistoryInputsType = z.infer<typeof MaternalMedicalHistoryInputs>;
+type MaternalMedicalHistoryInputs = z.infer<typeof MaternalMedicalHistoryInputs>;
 
 const MaternalMedicalHistoryResponse = MaternalMedicalHistoryInputs.extend({
     id: z.string(),
@@ -36,7 +50,7 @@ const MaternalMedicalHistoryResponse = MaternalMedicalHistoryInputs.extend({
 export default function MaternalMedicalHistory() {
 
 
-    const { register, control, handleSubmit, formState: { errors } } = useForm<MaternalMedicalHistoryInputsType>({
+    const { register, control, handleSubmit, formState: { errors }, setValue } = useForm<MaternalMedicalHistoryInputs>({
         resolver: zodResolver(MaternalMedicalHistoryInputs),
         defaultValues: {
             current_medication_list: [],
@@ -60,7 +74,25 @@ export default function MaternalMedicalHistory() {
         })
     };
 
-    const { mutate } = useMutation(async (data: MaternalMedicalHistoryInputsType) => {
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const response = await axios.get('http://127.0.0.1:5000/api/get_maternal_medical_history/d2bd4688-5527-4bbb-b1a8-af1399d00b12')
+                const userData = response.data;
+                Object.keys(userData).forEach(key => {
+                    if (key !== 'id' && key !== 'user_id') {
+                        const formKey = key as keyof MaternalMedicalHistoryInputs;
+                        setValue(formKey, userData[key]);
+                    }
+                });
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
+        fetchUserData();
+    }, []);
+
+    const { mutate } = useMutation(async (data: MaternalMedicalHistoryInputs) => {
         const { data: responseData } = (await axios.post('http://127.0.0.1:5000/api/add_maternal_medical_history', { ...data, user_id: "d2bd4688-5527-4bbb-b1a8-af1399d00b12" }));
 
         MaternalMedicalHistoryResponse.parse(responseData);
@@ -75,12 +107,13 @@ export default function MaternalMedicalHistory() {
         }
     });
 
-
-    const deliveryModes = ["Vaginal", "Cesarean"];
-
     return (
         <div className="flex justify-center w-full p-2 mt-2 text-base font-OpenSans">
             <form onSubmit={handleSubmit((data) => mutate(data))} className="w-[40rem] md:w-[30rem] m-5 md:m-0 space-y-1 [&>p]:pt-6 [&>p]:pb-1 [&>input]:px-4">
+
+                <p className="font-semibold text-red-700">Complete with OB/GYN or Primary Care Provider</p>
+                <div className="w-full h-px bg-gray-300"></div>
+                
                 <p className="font-medium text-xl whitespace-nowrap">Prenatal Care (for current or most recent pregnancy)</p>
 
                 <p className="font-medium">Gestational Age at Entry of Care</p>
@@ -113,14 +146,11 @@ export default function MaternalMedicalHistory() {
 
                 <p className="font-medium">Attended Postpartum Visit</p>
                 <div className="flex flex-col space-y-2">
-                    <label className="inline-flex items-center">
-                        <input {...register("attended_postpartum_visit", { required: true })} type="radio" value="true" className="form-radio" />
-                        <span className="ml-2">Yes</span>
-                    </label>
-                    <label className="inline-flex items-center">
-                        <input {...register("attended_postpartum_visit")} type="radio" value='' className="form-radio" />
-                        <span className="ml-2">No</span>
-                    </label>
+                    {attendedPostpartum.map((status) => (
+                        <label key={status} className="inline-flex items-center">
+                            <input {...register("attended_postpartum_visit")} type="radio" value={status} className="form-radio" />
+                            <span className="ml-2">{status}</span>
+                        </label>))}
                     {errors.attended_postpartum_visit && <span className="label-text-alt text-red-500">{errors.attended_postpartum_visit.message}</span>}
                 </div>
 
