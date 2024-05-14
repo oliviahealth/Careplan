9529795db391125a8a892f5e078f8e02c744149c
Anirudh Nukala <anirudh.nukala@gmail.com>
2024-05-13 22:57:02 -0500

    changed color of boxes and removed shadows

--- src/components/Dashboard/FormSelector.tsx
+++ src/components/Dashboard/FormSelector.tsx
@@ -662,7 +662,7 @@ const FormSelector: React.FC<FormSelectorProps> = ({
     return (
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 py-2">
         <div className="col-span-2"></div>
-        <div className="shadow-md rounded-lg p-4 bg-gray-100">
+        <div className="  rounded-lg p-4 bg-neutral-100">
           <h3 className="text-lg mb-4" style={{ color: '#797474' }}>Personal Information</h3>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {personalInformationFields.map(key => {
@@ -679,7 +679,7 @@ const FormSelector: React.FC<FormSelectorProps> = ({
             })}
           </div>
         </div>
-        <div className="shadow-md rounded-lg p-4 bg-gray-100" >
+        <div className="  rounded-lg p-4 bg-neutral-100" >
           <h3 className="text-lg mb-4" style={{ color: '#797474' }}>Contact</h3>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <div>
@@ -700,7 +700,7 @@ const FormSelector: React.FC<FormSelectorProps> = ({
             })}
           </div>
         </div>
-        <div className="shadow-md rounded-lg p-4 bg-gray-100">
+        <div className="  rounded-lg p-4 bg-neutral-100">
           <h3 className="text-lg mb-4" style={{ color: '#797474' }}>Insurance</h3>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {insuranceFields.map(key => {
@@ -717,7 +717,7 @@ const FormSelector: React.FC<FormSelectorProps> = ({
             })}
           </div>
         </div>
-        <div className="shadow-md rounded-lg p-4 bg-gray-100">
+        <div className="  rounded-lg p-4 bg-neutral-100">
           <h3 className="text-lg mb-4" style={{ color: '#797474' }}>Emergency Contact</h3>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {emergencyContactFields.map(key => {
@@ -786,7 +786,7 @@ const FormSelector: React.FC<FormSelectorProps> = ({
   
     return (
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 py-2">
-        <div className="shadow-md rounded-lg p-4 bg-gray-100">
+        <div className="  rounded-lg p-4 bg-neutral-100">
           <h3 className="text-lg mb-4" style={{ color: '#797474' }}>Prenatal Care</h3>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {prenatalCareFields.map(key => {
@@ -804,7 +804,7 @@ const FormSelector: React.FC<FormSelectorProps> = ({
             })}
           </div>
         </div>
-        <div className="shadow-md rounded-lg p-4 bg-gray-100">
+        <div className="  rounded-lg p-4 bg-neutral-100">
           <h3 className="text-lg mb-4" style={{ color: '#797474' }}>Obstetric History</h3>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {obstetricHistoryFields.map(key => {
@@ -819,7 +819,7 @@ const FormSelector: React.FC<FormSelectorProps> = ({
             })}
           </div>
         </div>
-        <div className="shadow-md rounded-lg p-4 bg-gray-100">
+        <div className="  rounded-lg p-4 bg-neutral-100">
           <h3 className="text-lg mb-4" style={{ color: '#797474' }}>Medical Problems</h3>
           <div>
             {medicalProblemsFields.map(key => {
@@ -838,7 +838,7 @@ const FormSelector: React.FC<FormSelectorProps> = ({
             </div>
           </div>
         </div>
-        <div className="shadow-md rounded-lg p-4 bg-gray-100">
+        <div className="  rounded-lg p-4 bg-neutral-100">
           <h3 className="text-lg mb-4" style={{ color: '#797474' }}>Current Medication List</h3>
           <div>
             {medicationList.map((medication: Medication, index: number) => (
