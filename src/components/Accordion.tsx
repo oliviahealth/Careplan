da2112a0980812ce6c779fe0df5eefc4b21cb687
Kunal Jain <55034053+wherewith@users.noreply.github.com>
2024-05-14 22:05:59 -0500

    fix highlight accordion on open

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -35,7 +35,7 @@ const Accordion: React.FC<AccordionProps> = ({
           setIsOpen(!isOpen);
           onClick && onClick();
         }}
-        className={`w-full h-fit py-3 px-6 rounded-2xl bg-neutral-100 flex justify-between items-center text-lg border-2 border-transparent transition duration-100 ease-in ${isOpen ? "border-[#5D1B2A]" : ""}`}
+        className={`w-full h-fit py-3 px-6 rounded-2xl bg-neutral-100 flex justify-between items-center text-lg border-2 transition duration-100 ease-in ${isOpen ? "border-[#5D1B2A]" : "border-transparent "}`}
       >
         {title}
         <div className="flex flex-row gap-10">
